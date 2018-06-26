20c240cfea42fd1c19db7ac5ded6e92094c40d44
gnrliubin <liubin@wxjt.com.cn>
2018-06-26 22:00:38 +0800

    20180626-HOME

---
+++ src/components/layout/Layout.vue
@@ -0,0 +1,68 @@
+<template>
+    <div id="layout">
+        <el-container>
+            <el-header>
+                <Nav></Nav>
+            </el-header>
+            <el-container>
+                <el-aside width="200px">
+                    <aside-menu></aside-menu>
+                </el-aside>
+                <el-main>Main</el-main>
+            </el-container>
+        </el-container>
+    </div>
+</template>
+
+<script>
+    import {Nav,AsideMenu} from './subcomponents'
+    export default  {
+        name:'Layout',
+        components:{
+            Nav,
+            AsideMenu
+        }
+    }
+</script>
+<style scoped>
+    .el-header, .el-footer {
+    background-color: #3775f7;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+  
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+  
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+
+    #layout{
+        height:100%
+    }
+    .el-container {  
+        height: 100%;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import store from '../store'
+import Layout from '@/components/layout/Layout'
+
 
 Vue.use(Router)
 
@@ -8,23 +10,21 @@ Vue.use(Router)
   routes: [
     {
       path: '/',
-      name: '',
-      components:{ 
-        
-      },
-      beforeEnter:(to,from,next)=>{
-        alert('路由独享守卫')
-      }
+      name: 'Layout',
+      component: Layout,
+      // beforeEnter:(to,from,next)=>{
+      //   alert('路由独享守卫')
+      // }
     }
   ]
 })
 
-// 全局守卫会覆盖独享守卫
-router.beforeEach((to,from,next)=>{
-  alert('路由全局守卫')
-  store.commit('increment')
-  console.log(this) //这个this是router
-  console.log(this.$store)  //undefined
-})
+// // 全局守卫会覆盖独享守卫
+// router.beforeEach((to,from,next)=>{
+//   alert('路由全局守卫')
+//   store.commit('increment')
+//   console.log(this) //这个this是router
+//   console.log(this.$store)  //undefined
+// })
 
 export default router
